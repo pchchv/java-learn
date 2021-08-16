98b7b628527b62d044a33f14f2c0c84c04c4bbd7
Jack <github@jackio.me>
2021-08-16 09:15:01 +0300

    Started coding the bubble sorting algorithm

---
+++ algorithms/BubleSort/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[] {64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+        printArray(array);
+        for (int i = 1; i < array.length; i++)  {
+            if (array[i] < array[i - 1]) {
+                int temp = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = temp;
+            }
+        }
+    }
+    private static void printArray(int[] arr) {
+        System.out.print("|");
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(arr[i]);
+        }
+    }
+}
