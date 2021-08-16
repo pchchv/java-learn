57b90e4f14defb7fda1dbb6a40c34da56275a2ee
Jack <github@jackio.me>
2021-08-16 09:46:49 +0300

    Bubble sort algorithm

--- algorithms/BubleSort/src/Main.java
+++ algorithms/BubleSort/src/Main.java
@@ -1,22 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[] {64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+        int[] array = new int[]{64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
         printArray(array);
-        for (int i = 1; i < array.length; i++)  {
-            if (array[i] < array[i - 1]) {
-                int temp = array[i];
-                array[i] = array[i - 1];
-                array[i - 1] = temp;
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    isSorted = false;
+                }
             }
+            printArray(array);
         }
     }
     private static void printArray(int[] arr) {
-        System.out.print("|");
+        System.out.print("[");
         for (int i = 0; i < arr.length; i++) {
             if (i > 0) {
                 System.out.print(", ");
             }
             System.out.print(arr[i]);
         }
+        System.out.println("]");
     }
 }
