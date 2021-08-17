286d002cd7c4d05d8a87ffc27e6b3aa8e427693e
Jack <github@jackio.me>
2021-08-17 07:44:55 +0300

    Quick sort algorithm

---
+++ algorithms/QuickSort/src/Main.java
@@ -0,0 +1,58 @@
+public class Main {
+    /**
+     * Quick sorting algorithm
+     * @param cmdLineArgs command-line arguments
+     */
+    public static void main(String[] args) {
+        int[] array = new int[] {64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int from, int to) {
+        System.out.println(arrayToString(arr));
+        if (from < to) {
+            int divideIndex = partition(arr, from, to);
+            quickSort(arr, from, divideIndex - 1);
+            quickSort(arr, divideIndex, to);
+        }
+    }
+
+    private static int partition(int[] arr, int from, int to) {
+        int leftIndex = from;
+        int rightIndex = to;
+        int pivot = arr[from + (to - from) / 2];
+        while (leftIndex <= rightIndex) {
+            while (arr[leftIndex] < pivot) {
+                leftIndex++;
+            }
+            while (arr[rightIndex] > pivot) {
+                rightIndex--;
+            }
+            if (leftIndex <= rightIndex) {
+                swap(arr, rightIndex, leftIndex);
+                leftIndex++;
+                rightIndex--;
+            }
+        }
+        return leftIndex;
+    }
+
+    private static void swap(int[] array, int index0, int index1) {
+        int temp = array[index0];
+        array[index0] = array[index1];
+        array[index1] = temp;
+    }
+
+    private static String arrayToString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(array[i]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
