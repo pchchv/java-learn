f565f6b02670a16190c9c1d922dc86ee9e5784ca
Jack <github@jackio.me>
2021-08-17 05:46:34 +0300

    Searching for files in folders on disk. Final

--- various/FileSearch/src/ru/arhiser/Main.java
+++ various/FileSearch/src/ru/arhiser/Main.java
@@ -1,7 +1,38 @@
 package ru.arhiser;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    /**
+     * The program searches for files in folders on the disk
+     *
+     * @param cmdLineArgs command-lene arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<File> fileList = new ArrayList<>();
+        searchFiles(new File("/home"), fileList);
+        for(File file: fileList) {
+            System.out.println(file.getAbsolutePath());
+        }
+    }
+
+    private static void searchFiles(File rootFile, List<File> fileList) {
+        if (rootFile.isDirectory()) {
+            System.out.println("searching at: " + rootFile.getAbsolutePath());
+            File[] directoryFiles = rootFile.listFiles();
+            if (directoryFiles != null) {
+                for(File file: directoryFiles) {
+                    if (file.isDirectory()) {
+                        searchFiles(file, fileList);
+                    } else {
+                        if (file.getName().toLowerCase().endsWith(".jpg")) {
+                            fileList.add(file);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
