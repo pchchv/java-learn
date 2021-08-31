e225e1e6b3cd853b7f483bdb26627e20a8478ac1
Jack <github@jackio.me>
2021-09-01 00:20:30 +0300

    Stack

---
+++ algorithms/Stack&Queue/src/Stack.java
@@ -0,0 +1,5 @@
+public interface Stack<T> {
+    void push(T item);    // Добавление элемента на вершину стека
+    T pop();              // Изъятие элемента с вершины стека
+    boolean isEmpty();
+}
